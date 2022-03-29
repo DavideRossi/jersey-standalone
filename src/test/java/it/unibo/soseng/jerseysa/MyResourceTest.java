3fd166694f1ed67876bc15a625e19663dc2d2c2d
Davide Rossi <daviderossi@unibo.it>
2022-03-29 22:17:53 +0200

    FIX test

--- src/test/java/it/unibo/soseng/jerseysa/MyResourceTest.java
+++ src/test/java/it/unibo/soseng/jerseysa/MyResourceTest.java
@@ -1,4 +1,4 @@
-package it.unibo.soseng.rssa.sarecipe;
+package it.unibo.soseng.jerseysa;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
@@ -9,8 +9,6 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import it.unibo.soseng.jerseysa.Main;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,28 +22,21 @@ public class MyResourceTest {
         // start the server
         server = Main.startServer();
         // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
+        Client client = ClientBuilder.newClient();
+        target = client.target(Main.BASE_URI);
     }
 
     @After
     public void tearDown() throws Exception {
-        server.stop();
+        server.shutdownNow();
     }
 
     /**
      * Test to see that the message "Got it!" is sent in the response.
      */
     @Test
-    public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
-        assertEquals("Got it!", responseMsg);
+    public void testHello() {
+        String responseMsg = target.path("hello").queryParam("name", "Davide").request().get(String.class);
+        assertEquals("Hello Davide", responseMsg);
     }
 }
